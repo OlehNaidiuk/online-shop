6974d55a46ce7cfd5d95b7525fbbb4d49d345a1f
OlehNaidiuk <olegnaidjuk@gmail.com>
2022-08-07 17:15:04 +0300

    Add logs for controllers

--- src/main/java/com/naidiuk/onlineshop/controller/CategoryController.java
+++ src/main/java/com/naidiuk/onlineshop/controller/CategoryController.java
@@ -3,22 +3,33 @@ package com.naidiuk.onlineshop.controller;
 import com.naidiuk.onlineshop.dto.CategoryDto;
 import com.naidiuk.onlineshop.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/categories")
 @RequiredArgsConstructor
+@Slf4j
 public class CategoryController {
     private final CategoryService categoryService;
+    @Autowired
+    private HttpServletRequest request;
 
     @GetMapping
     public ResponseEntity<List<CategoryDto>> findAll() {
+        log.info("Request URL: " + request.getRequestURL() + ", "
+                + "Host: " + request.getRemoteHost() + ", "
+                + "Address: " + request.getRemoteAddr() + ", "
+                + "Request URI: " + request.getRequestURI() + ", "
+                + "Request params: " + request.getQueryString() + ".");
         List<CategoryDto> categoriesDto = categoryService.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(categoriesDto);
     }
--- src/main/java/com/naidiuk/onlineshop/controller/ProductController.java
+++ src/main/java/com/naidiuk/onlineshop/controller/ProductController.java
@@ -3,22 +3,33 @@ package com.naidiuk.onlineshop.controller;
 import com.naidiuk.onlineshop.dto.ProductDto;
 import com.naidiuk.onlineshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@Slf4j
 public class ProductController {
     private final ProductService productService;
+    @Autowired
+    private HttpServletRequest request;
 
     @GetMapping("/products/random")
     public ResponseEntity<List<ProductDto>> findTenRandom() {
+        log.info("Request URL: " + request.getRequestURL() + ", "
+                + "Host: " + request.getRemoteHost() + ", "
+                + "Address: " + request.getRemoteAddr() + ", "
+                + "Request URI: " + request.getRequestURI() + ", "
+                + "Request params: " + request.getQueryString() + ".");
         List<ProductDto> productsDto = productService.findTenRandom();
         return ResponseEntity.status(HttpStatus.OK).body(productsDto);
     }

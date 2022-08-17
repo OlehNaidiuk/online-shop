421a5c48a5f548eaeb7f51d5e9e79daba8732e29
OlehNaidiuk <olegnaidjuk@gmail.com>
2022-08-17 15:51:00 +0300

    Fix mappers

--- src/main/java/com/naidiuk/onlineshop/mapper/CompanyMapper.java
+++ src/main/java/com/naidiuk/onlineshop/mapper/CompanyMapper.java
@@ -1,6 +1,7 @@
 package com.naidiuk.onlineshop.mapper;
 
 import com.naidiuk.onlineshop.dto.CompanyDto;
+import com.naidiuk.onlineshop.dto.CompanyProductsDto;
 import com.naidiuk.onlineshop.dto.ProductDto;
 import com.naidiuk.onlineshop.entity.Company;
 import com.naidiuk.onlineshop.entity.Product;
@@ -9,23 +10,24 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CompanyMapper {
+
     public static CompanyDto transformToDto(Company company) {
-        List<Product> products = company.getProducts();
-        List<ProductDto> productsDto;
-        productsDto = products.stream()
-                            .map(ProductMapper::transformToDto)
-                            .collect(Collectors.toList());
         return CompanyDto.builder()
                 .companyId(company.getCompanyId())
                 .name(company.getName())
-                .products(productsDto)
                 .build();
     }
 
-    public static CompanyDto transformToDtoWithoutProducts(Company company) {
-        return CompanyDto.builder()
+    public static CompanyProductsDto transformToDtoWithProducts(Company company) {
+        List<Product> products = company.getProducts();
+        List<ProductDto> productsDto = products.stream()
+                                            .map(ProductMapper::transformToDto)
+                                            .collect(Collectors.toList());;
+
+        return CompanyProductsDto.builder()
                 .companyId(company.getCompanyId())
                 .name(company.getName())
+                .products(productsDto)
                 .build();
     }
 }

0057c086011a08e5d272b201097e7be0b32f401a
OlehNaidiuk <olegnaidjuk@gmail.com>
2022-08-17 15:51:59 +0300

    Fix CompanyRepository

--- src/main/java/com/naidiuk/onlineshop/repository/CompanyRepository.java
+++ src/main/java/com/naidiuk/onlineshop/repository/CompanyRepository.java
@@ -3,9 +3,5 @@ package com.naidiuk.onlineshop.repository;
 import com.naidiuk.onlineshop.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface CompanyRepository extends JpaRepository<Company, Long> {
-    @Override
-    Optional<Company> findById(Long companyId);
 }

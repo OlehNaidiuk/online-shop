d7c5b669304ad5f0528ee765be7e68481e593928
OlehNaidiuk <olegnaidjuk@gmail.com>
2022-08-29 20:05:58 +0300

    Add specification to product repository

--- src/main/java/com/naidiuk/onlineshop/repository/ProductRepository.java
+++ src/main/java/com/naidiuk/onlineshop/repository/ProductRepository.java
@@ -2,11 +2,14 @@ package com.naidiuk.onlineshop.repository;
 
 import com.naidiuk.onlineshop.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>,
+                                            JpaSpecificationExecutor<Product> {
+
     @Query(value = "SELECT * FROM product ORDER BY random() LIMIT 10", nativeQuery = true)
     List<Product> findTenRandom();
 }

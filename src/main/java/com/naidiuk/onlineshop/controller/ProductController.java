af5c94c846a7b1c84345dc017016f6bbbf6127d7
OlehNaidiuk <olegnaidjuk@gmail.com>
2022-08-31 15:56:59 +0300

    Add method findAllByFilters for ProductController

--- src/main/java/com/naidiuk/onlineshop/controller/ProductController.java
+++ src/main/java/com/naidiuk/onlineshop/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.naidiuk.onlineshop.controller;
 
+import com.naidiuk.onlineshop.dto.ProductCategorySizesDto;
 import com.naidiuk.onlineshop.dto.ProductDto;
+import com.naidiuk.onlineshop.entity.Color;
 import com.naidiuk.onlineshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -29,6 +32,26 @@ public class ProductController {
                 , request.getRequestURI()
                 , request.getQueryString());
         List<ProductDto> productsDto = productService.findTenRandom();
+        return ResponseEntity.status(HttpStatus.OK).body(productsDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findAllByFilters(
+                    HttpServletRequest request,
+                    @RequestParam(name = "category", required = false) List<Long> categoryIds,
+                    @RequestParam(name = "size", required = false) List<Long> sizeIds,
+                    @RequestParam(name = "color", required = false) List<Color> colors,
+                    @RequestParam(defaultValue = "0") int minPrice,
+                    @RequestParam(defaultValue = "100000") int maxPrice) {
+
+        log.info("Request URL: {}, Host: {}, Address: {}, Request URI: {}, Request params: {}."
+                , request.getRequestURL()
+                , request.getRemoteHost()
+                , request.getRemoteAddr()
+                , request.getRequestURI()
+                , request.getQueryString());
+        List<ProductCategorySizesDto> productsDto =
+                productService.findAllByFilters(categoryIds, sizeIds, colors, minPrice, maxPrice);
         return ResponseEntity.status(HttpStatus.OK).body(productsDto);
     }
 }

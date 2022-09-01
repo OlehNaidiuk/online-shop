b94c50941a3807be62fa93ea34fbf2c3032b174a
OlehNaidiuk <olegnaidjuk@gmail.com>
2022-09-01 12:26:27 +0300

    Refactor method findAllBy of ProductController

--- src/main/java/com/naidiuk/onlineshop/controller/ProductController.java
+++ src/main/java/com/naidiuk/onlineshop/controller/ProductController.java
@@ -1,8 +1,8 @@
 package com.naidiuk.onlineshop.controller;
 
+import com.naidiuk.onlineshop.dto.ProductFilterDto;
 import com.naidiuk.onlineshop.dto.ProductCategorySizesDto;
 import com.naidiuk.onlineshop.dto.ProductDto;
-import com.naidiuk.onlineshop.entity.Color;
 import com.naidiuk.onlineshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -36,22 +35,14 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<?> findAllByFilters(
-                    HttpServletRequest request,
-                    @RequestParam(name = "category", required = false) List<Long> categoryIds,
-                    @RequestParam(name = "size", required = false) List<Long> sizeIds,
-                    @RequestParam(name = "color", required = false) List<Color> colors,
-                    @RequestParam(defaultValue = "0") int minPrice,
-                    @RequestParam(defaultValue = "100000") int maxPrice) {
-
+    public ResponseEntity<?> findAllBy(HttpServletRequest request, ProductFilterDto productFilter) {
         log.info("Request URL: {}, Host: {}, Address: {}, Request URI: {}, Request params: {}."
                 , request.getRequestURL()
                 , request.getRemoteHost()
                 , request.getRemoteAddr()
                 , request.getRequestURI()
                 , request.getQueryString());
-        List<ProductCategorySizesDto> productsDto =
-                productService.findAllByFilters(categoryIds, sizeIds, colors, minPrice, maxPrice);
+        List<ProductCategorySizesDto> productsDto = productService.findAllBy(productFilter);
         return ResponseEntity.status(HttpStatus.OK).body(productsDto);
     }
 }

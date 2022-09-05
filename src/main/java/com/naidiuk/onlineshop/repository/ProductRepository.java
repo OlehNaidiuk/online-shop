e6ae6700720630de64d553bdeb485a5a4eda62c1
OlehNaidiuk <olegnaidjuk@gmail.com>
2022-09-05 12:46:27 +0300

    Add method findByIdWithReviews for ProductRepository

--- src/main/java/com/naidiuk/onlineshop/repository/ProductRepository.java
+++ src/main/java/com/naidiuk/onlineshop/repository/ProductRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long>,
                                             JpaSpecificationExecutor<Product> {
 
     @Query(value = "SELECT * FROM product ORDER BY random() LIMIT 10", nativeQuery = true)
     List<Product> findTenRandom();
+    @Query(value = "SELECT p FROM Product p LEFT JOIN FETCH p.reviews WHERE p.productId=:productId")
+    Optional<Product> findByIdWithReviews(Long productId);
 }

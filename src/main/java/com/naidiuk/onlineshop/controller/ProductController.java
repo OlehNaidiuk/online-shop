d000bba09bf68424df24a1b9abf7f3662a2933bc
OlehNaidiuk <olegnaidjuk@gmail.com>
2022-09-09 13:24:17 +0300

    Add methhods save, update, delete

--- src/main/java/com/naidiuk/onlineshop/controller/ProductController.java
+++ src/main/java/com/naidiuk/onlineshop/controller/ProductController.java
@@ -1,16 +1,15 @@
 package com.naidiuk.onlineshop.controller;
 
-import com.naidiuk.onlineshop.dto.ProductFilterDto;
+import com.naidiuk.onlineshop.dto.ProductAllDto;
 import com.naidiuk.onlineshop.dto.ProductCategorySizesDto;
 import com.naidiuk.onlineshop.dto.ProductDto;
+import com.naidiuk.onlineshop.dto.ProductFilterDto;
 import com.naidiuk.onlineshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -44,5 +43,41 @@ public class ProductController {
                 , request.getQueryString());
         List<ProductCategorySizesDto> productsDto = productService.findAllBy(productFilter);
         return ResponseEntity.status(HttpStatus.OK).body(productsDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(HttpServletRequest request, @RequestBody ProductAllDto productAllDto) {
+        log.info("Request URL: {}, Host: {}, Address: {}, Request URI: {}, Request params: {}."
+                , request.getRequestURL()
+                , request.getRemoteHost()
+                , request.getRemoteAddr()
+                , request.getRequestURI()
+                , request.getQueryString());
+        ProductAllDto savedProductDto = productService.save(productAllDto);
+        return ResponseEntity.status(HttpStatus.OK).body(savedProductDto);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(HttpServletRequest request, @RequestBody ProductDto productDtoToUpdate) {
+        log.info("Request URL: {}, Host: {}, Address: {}, Request URI: {}, Request params: {}."
+                , request.getRequestURL()
+                , request.getRemoteHost()
+                , request.getRemoteAddr()
+                , request.getRequestURI()
+                , request.getQueryString());
+        ProductDto updatedProductDto = productService.update(productDtoToUpdate);
+        return ResponseEntity.status(HttpStatus.OK).body(updatedProductDto);
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<?> delete(HttpServletRequest request, @PathVariable Long productId) {
+        log.info("Request URL: {}, Host: {}, Address: {}, Request URI: {}, Request params: {}."
+                , request.getRequestURL()
+                , request.getRemoteHost()
+                , request.getRemoteAddr()
+                , request.getRequestURI()
+                , request.getQueryString());
+        ProductDto deletedProductDto = productService.deleteById(productId);
+        return ResponseEntity.status(HttpStatus.OK).body(deletedProductDto);
     }
 }

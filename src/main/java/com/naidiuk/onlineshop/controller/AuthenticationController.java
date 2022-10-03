02fa998ffb26d82dc741d801a4a53aef908c4d81
OlehNaidiuk <olegnaidjuk@gmail.com>
2022-10-03 16:34:29 +0300

    Refactor authentication controller

--- src/main/java/com/naidiuk/onlineshop/controller/AuthenticationController.java
+++ src/main/java/com/naidiuk/onlineshop/controller/AuthenticationController.java
@@ -1,14 +1,11 @@
 package com.naidiuk.onlineshop.controller;
 
+import com.naidiuk.onlineshop.dto.AuthenticationDto;
 import com.naidiuk.onlineshop.dto.AuthenticationRequestDto;
-import com.naidiuk.onlineshop.entity.User;
-import com.naidiuk.onlineshop.security.JwtTokenProvider;
-import com.naidiuk.onlineshop.service.UserService;
+import com.naidiuk.onlineshop.security.SecurityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,26 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
-    private final UserService userService;
-    private final AuthenticationManager authenticationManager;
-    private final JwtTokenProvider jwtTokenProvider;
+    private final SecurityService securityService;
 
     @PostMapping("/login")
     public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequestDto authRequestDto) {
-        User user = userService.findByUsername(authRequestDto.getUsername());
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequestDto.getUsername(), authRequestDto.getPassword()));
-        String token = jwtTokenProvider.createToken(user.getUsername(), user.getRole().name());
-        Map<String, Object> response = new HashMap<>();
-        response.put("username", user.getUsername());
-        response.put("token", token);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        AuthenticationDto authDto = securityService.getAuthentication(authRequestDto);
+        return ResponseEntity.status(HttpStatus.OK).body(authDto);
     }
 
     @PostMapping("/logout")

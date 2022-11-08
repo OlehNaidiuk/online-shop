a8e584f338d82f55d0b87b2412a6f24c53a71ccd
OlehNaidiuk <olegnaidjuk@gmail.com>
2022-11-08 18:15:06 +0200

    Add statistics repository

---
+++ src/main/java/com/naidiuk/onlineshop/repository/StatisticsRepository.java
@@ -0,0 +1,7 @@
+package com.naidiuk.onlineshop.repository;
+
+import com.naidiuk.onlineshop.entity.Statistics;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StatisticsRepository extends JpaRepository<Statistics, Long> {
+}

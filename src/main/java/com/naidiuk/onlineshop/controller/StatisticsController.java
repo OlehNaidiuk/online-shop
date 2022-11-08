1679a845b79b7cb3172cddaf70047ced4507ffb7
OlehNaidiuk <olegnaidjuk@gmail.com>
2022-11-08 18:18:12 +0200

    Add statistics controller

---
+++ src/main/java/com/naidiuk/onlineshop/controller/StatisticsController.java
@@ -0,0 +1,34 @@
+package com.naidiuk.onlineshop.controller;
+
+import com.naidiuk.onlineshop.dto.StatisticsDto;
+import com.naidiuk.onlineshop.service.StatisticsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/statistics")
+@RequiredArgsConstructor
+public class StatisticsController {
+    private final StatisticsService statisticsService;
+
+    @GetMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<?> findAllAndSort(
+            @RequestParam(required = false, defaultValue = "asc") String sortBy) {
+        List<StatisticsDto> productsStatistics;
+        if (sortBy.equals("asc")) {
+            productsStatistics = statisticsService.getProductsStatisticsSortedByAscending();
+        } else {
+            productsStatistics = statisticsService.getProductsStatisticsSortedByDescending();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(productsStatistics);
+    }
+}
